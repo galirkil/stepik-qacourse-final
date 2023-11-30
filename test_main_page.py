cb089c4de7fea07f655c473ef668d890e61db2a2
galirkil <galirkil@gmail.com>
2023-12-01 01:52:56 +0300

    Add go to login page step

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(driver):
@@ -6,3 +7,5 @@ def test_guest_can_go_to_login_page(driver):
     page = MainPage(driver, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(driver, driver.current_url)
+    login_page.should_be_login_page()
